717423d955d854da12afb25e05a582f628ed87b2
sirawt <31649128+MASDXI@users.noreply.github.com>
2025-12-07 19:02:32 +0700

    chore: format comment

--- contracts/interfaces/compliance/IFrozenRegistry.sol
+++ contracts/interfaces/compliance/IFrozenRegistry.sol
@@ -15,15 +15,15 @@ interface IFrozenRegistry {
     /// @dev Is given the account address is registered as merchant.
     function isFrozen(address account) external view returns (bool);
 
-    ///@dev add an address to the frozenlist
-    ///Throws unless `msg.sender` is an Issuer operator
-    ///@param _operator address to add
-    ///@return true if the address was added to the frozenlist, false if the address was already in the frozenlist
+    /// @dev add an address to the frozenlist
+    /// Throws unless `msg.sender` is an Issuer operator
+    /// @param _operator address to add
+    /// @return true if the address was added to the frozenlist, false if the address was already in the frozenlist
     function addAddressToFrozenlist(address _operator) external returns (bool);
 
-    ///@dev remove an address from the frozenlist
-    ///Throws unless `msg.sender` is an Issuer operator
-    ///@param _operator address to remove
-    ///@return true if the address was removed from the frozenlist, false if the address wasn't in the frozenlist in the first place
+    /// @dev remove an address from the frozenlist
+    /// Throws unless `msg.sender` is an Issuer operator
+    /// @param _operator address to remove
+    /// @return true if the address was removed from the frozenlist, false if the address wasn't in the frozenlist in the first place
     function removeAddressFromFrozenlist(address _operator) external returns (bool);
 }
